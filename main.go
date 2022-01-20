b5416c813559fbcb9c8c575fea00fcbc0ae29c9d
sopop <sopop@126.com>
2022-01-20 16:10:09 +0800

    update reminder

--- main.go
+++ main.go
@@ -4,6 +4,14 @@ import (
 	"demo/router"
 )
 
+// @title go基础项目
+// @version 1.0
+// @description go 基础项目 api docs.
+// @license.name Apache 2.0
+// @contact.name go-swagger帮助文档
+// @contact.url https://github.com/swaggo/swag/blob/master/README_zh-CN.md
+// @host localhost:8089
+// @BasePath /api
 func main() {
 	router := router.Strat()
 	router.Run(":8089")
